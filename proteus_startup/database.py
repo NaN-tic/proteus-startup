7b24cdf1b752937f6622a7088772f03b5c4cdc9e
??ngel ??lvarez <angel@nan-tic.com>
2015-10-21 17:31:51 +0200

    add connect_database funcion

--- proteus_startup/database.py
+++ proteus_startup/database.py
@@ -1,8 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from proteus import config as pconfig
-
-__all__ = ['init_database', 'drop_database']
+import os
+__all__ = ['init_database', 'drop_database', 'connect_database']
 
 
 def init_database():
@@ -16,3 +16,8 @@ def init_database():
 def drop_database():
     from trytond.tests.test_tryton import doctest_dropdb
     doctest_dropdb(None)
+
+
+def connect_database(database, password='admin', database_type='postgresql'):
+    return pconfig.set_trytond(database, password=password,
+        config_file=os.environ.get('TRYTOND_CONFIG', 'trytond.conf'))
