3d16168a1e4b976124ce354c8e480d841094d883
Guillem Barba <guillem@nan-tic.com>
2015-11-19 13:22:36 +0100

    Add get_asset method

--- proteus_startup/asset.py
+++ proteus_startup/asset.py
@@ -1,9 +1,36 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+from proteus import Model
+
 from .common import create_model
 
 
-__all__ = ['create_asset']
+__all__ = ['get_asset', 'create_asset']
+
+
+def get_asset(type_, code=None, name=None):
+    Asset = Model.get('asset')
+
+    assert code or name, "Code ({}) or Name ({}) must be supplier".format(
+        code, name)
+
+    domain = [
+        ('type', '=', type_),
+        ]
+    vals = {
+        'type': type_,
+        }
+    if code:
+        domain.append(('code', '=', code))
+        vals['code'] = code
+    if name:
+        domain.append(('name', 'ilike', name))
+        vals['name'] = name
+
+    assets = Asset.find(domain)
+    if assets:
+        return assets[0]
+    return create_asset(**vals)
 
 
 @create_model('asset')
